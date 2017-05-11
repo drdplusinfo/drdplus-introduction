bfdb11661a9de32c6e7407764a4dd294fe451591
Jaroslav Týc <mail@jaroslavtyc.com>
2017-05-11 16:06:00 +0200

    Rolls and skills history

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
 $fileName = false;
-if ($_SERVER['QUERY_STRING']) {
-    $fileName = __DIR__ . '/' . $_SERVER['QUERY_STRING'] . '.html';
-    if (file_exists($fileName)) {
-        $handle = fopen($fileName, 'rb');
-        $firstRow = fgets($handle);
-        fclose($handle);
-        if (preg_match('~<h1>(?<title>[^<]+)</h1>~i', $firstRow, $matches)) {
-            $title = $matches['title'];
-        }
-    } else {
-        $fileName = false;
-    }
+$files = [
+    'patnactka' => ['file' => __DIR__ . '/patnactka.html', 'name' => 'Patnáctka'],
+    'poslove' => ['file' => __DIR__ . '/poslove.html', 'name' => 'Poslové'],
+    'rozhodnuti' => ['file' => __DIR__ . '/rozhodnuti.html', 'name' => 'Rozhodnutí'],
+    'hody_a_dovednosti' => ['file' => __DIR__ . '/hody_a_dovednosti.html', 'name' => 'Hody a dovednosti'],
+];
+if ($_SERVER['QUERY_STRING'] && array_key_exists($_SERVER['QUERY_STRING'], $files)) {
+    $fileName = $files[$_SERVER['QUERY_STRING']]['file'];
+    $title = $files[$_SERVER['QUERY_STRING']]['name'];
 }
 ?>
 <html>
@@ -26,9 +23,9 @@ if ($fileName) {
 } else {
     ?>
     <ul>
-        <li><a href="?poslove">Poslové</a></li>
-        <li><a href="?patnactka">Patnáctka</a></li>
-        <li><a href="?rozhodnuti">Rozhodnutí</a></li>
+        <?php foreach ($files as $key => $file) { ?>
+            <li><a href="?<?= $key ?>"><?= $file['name'] ?></a></li>
+        <?php } ?>
     </ul>
     <?php
 }
